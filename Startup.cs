eb8fa39b0992a65aadfaf34deeb6740cb5e7e7a6
ahmady016 <ahmady09@gmail.com>
2018-02-10 18:40:04 +0200

    adding config in startup and appsettings json file

--- Startup.cs
+++ Startup.cs
@@ -5,30 +5,45 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace AdventureWorks
 {
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+   public class Startup
+   {
+      public IConfiguration Config { get; private set; }
+      public Startup(IConfiguration config)
+      {
+         Config = config;
+      }
+      // This method gets called by the runtime. Use this method to add services to the container.
+      // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+      public void ConfigureServices(IServiceCollection services)
+      {
+         services.Configure<AppSettings>(Config.GetSection("AppSettings"));
+      }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+      // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+      public void Configure(IApplicationBuilder app,
+                            IHostingEnvironment env,
+                            IOptions<AppSettings> settings)
+      {
+         if (env.IsDevelopment())
+         {
+            app.UseDeveloperExceptionPage();
+         }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
-    }
+         app.Run(async (context) =>
+         {
+            await context.Response.WriteAsync($"SqlServerConnection: {settings.Value.SqlServerConnection}");
+         });
+      }
+
+      public class AppSettings
+      {
+         public string SqlServerConnection { get; set; }
+      }
+   }
 }
