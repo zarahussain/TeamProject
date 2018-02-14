112736af514f1e1b30b6f1f16344d7d52ac47e77
ahmady016 <ahmady09@gmail.com>
2018-02-14 04:24:59 +0200

    adding SetState Entension Method To DbContext

---
+++ Common/Extensions.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace 
+AdventureWorks
+{
+   public static class Extensions
+   {
+      public static void SetState<T>(this AdventureWorksContext context, T entity, string _state) where T : class
+      {
+         EntityState state;
+         switch (_state) {
+            case "Added":
+               state = EntityState.Added;
+               break;
+            case "Modified":
+               state = EntityState.Modified;
+               break;
+            case "Deleted":
+               state = EntityState.Deleted;
+               break;
+            default:
+               state = EntityState.Unchanged;
+               break;
+         }
+         context.Entry(entity).State = state;
+      }
+   }
+}
