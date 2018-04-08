16d810eace8d80a54f9b4460df007f1918ffb53f
ahmady016 <ahmady09@gmail.com>
2018-04-08 02:24:33 +0200

    Adding Swagger Restful API Helper

--- Startup.cs
+++ Startup.cs
@@ -8,55 +8,67 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace AdventureWorks
 {
-   public class Startup
-   {
-      public IConfiguration Config { get; private set; }
-      public Startup(IConfiguration config)
+  public class Startup
+  {
+    public IConfiguration Config { get; private set; }
+    public Startup(IConfiguration config)
+    {
+      Config = config;
+    }
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
+    {
+      // adding appSetting to DI
+      services.Configure<AppSettings>(Config.GetSection("AppSettings"));
+      // adding db context to DI
+      services.AddDbContext<AdventureWorksContext>();
+      // adding MVC
+      services.AddMvc()
+              .AddJsonOptions(options =>
+               {
+                 options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+               });
+      // Register the Swagger generator, defining one or more Swagger documents
+      services.AddSwaggerGen(c =>
       {
-         Config = config;
-      }
-      // This method gets called by the runtime. Use this method to add services to the container.
-      // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-      public void ConfigureServices(IServiceCollection services)
-      {
-         // adding appSetting to DI
-         services.Configure<AppSettings>(Config.GetSection("AppSettings"));
-         // adding db context to DI
-         services.AddDbContext<AdventureWorksContext>();
-         // adding MVC
-         services.AddMvc()
-                 .AddJsonOptions(options =>
-                  {
-                     options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-                  });
-      }
+        c.SwaggerDoc("v1", new Info { Title = "AdventureWords", Version = "v1" });
+      });
+    }
 
-      // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-      public void Configure(IApplicationBuilder app,
-                            IHostingEnvironment env,
-                            IOptions<AppSettings> settings)
-      {
-         if (env.IsDevelopment())
-         {
-            app.UseDeveloperExceptionPage();
-         }
-         // using static files with default files
-         app.UseFileServer();
-         // using mvc
-         app.UseMvcWithDefaultRoute();
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app,
+                          IHostingEnvironment env,
+                          IOptions<AppSettings> settings)
+    {
+      if (env.IsDevelopment())
+        app.UseDeveloperExceptionPage();
 
-         app.Run(async (context) =>
-         {
-            await context.Response.WriteAsync($"SqlServerConnection: {settings.Value.SqlServerConnection}");
-         });
-      }
-
-      public class AppSettings
+      // using static files with default files
+      app.UseFileServer();
+      // Enable middleware to serve generated Swagger as a JSON endpoint.
+      app.UseSwagger();
+      // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+      app.UseSwaggerUI(c =>
       {
-         public string SqlServerConnection { get; set; }
-      }
-   }
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "AdventureWords V1");
+      });
+      // using mvc
+      app.UseMvcWithDefaultRoute();
+      // finall request pipline fallback
+      app.Run(async (context) =>
+      {
+        await context.Response.WriteAsync($"SqlServerConnection: {settings.Value.SqlServerConnection}");
+      });
+    }
+
+    public class AppSettings
+    {
+      public string SqlServerConnection { get; set; }
+    }
+  }
 }
